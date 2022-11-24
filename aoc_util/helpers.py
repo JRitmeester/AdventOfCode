6ec0423595cd86b511aefb0d5c7c78b34b125191
JRitmeester <jeroen.c.ritmeester@gmail.com>
2022-11-24 16:58:27 +0100

    Update project templates and year setup

--- aoc_util/helpers.py
+++ aoc_util/helpers.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-from typing import List, Union
 from functools import wraps
 import time
 from aocd import get_data
+
 import numpy as np
 
 
@@ -12,9 +12,9 @@ def print_timing(func: callable) -> callable:
     use `@print_timing `just above the function you want to time
     """
 
-    @wraps(func)  # improves debugging
+    @wraps(func)
     def wrapper(*arg):
-        start = time.perf_counter()  # needs python3.3 or higher
+        start = time.perf_counter()
         result = func(*arg)
         end = time.perf_counter()
         print(f"{func.__name__} took {(end - start) * 1000:.8f} seconds")
@@ -25,7 +25,7 @@ def print_timing(func: callable) -> callable:
 
 def load_input_data(
     input_file: Path, day: int, year: int, split_by=None
-) -> Union[str, List[str]]:
+) -> str | list[str]:
     if not input_file.exists():
         data = get_data(day=day, year=year)
         input_file.write_text(data)
@@ -38,7 +38,7 @@ def load_input_data(
         return data
 
 
-def input_to_np_arr(input: str, dtype=None):
+def input_to_np_arr(input: str, dtype=None) -> np.ndarray:
     arr = np.array(input)
     if dtype:
         arr = arr.astype(dtype)
