108de3344b7ddfdfbdb82e2a9934d436256418c2
JRitmeester <jeroen.c.ritmeester@gmail.com>
2022-11-24 20:07:08 +0100

    Reformat Day 2/2021

--- 2021/Day 02/day02.py
+++ 2021/Day 02/day02.py
@@ -1,4 +1,17 @@
+import sys
 from pathlib import Path
+
+import numpy as np
+
+# Add the repository root the sys.path in order to import the helper modules.
+file = Path(__file__)
+REPO_ROOT = next(
+    (parent for parent in file.parents if parent.name.lower() == "advent of code"),
+    None,
+)
+sys.path.append(REPO_ROOT.as_posix())
+
+from aoc_util.helpers import load_input_data
 
 
 class Submarine:
@@ -30,21 +43,24 @@ class Submarine:
             getattr(self, command)(int(amount))
 
 
-def main():
-    instructions_path = Path(__file__).parent / "input.txt"
-    with instructions_path.open(mode="r") as file:
-        instructions = file.readlines()
+def preprocess_input(input_text: str):
+    return input_text.split("\n")
 
-    # Part 1
+
+def first(input) -> int:
     sub = Submarine(use_aim=False)
-    sub.parse_instructions(instructions)
-    print("Answer 1: ", sub.pos * sub.depth)
+    sub.parse_instructions(instructions=input)
+    return sub.pos * sub.depth
 
-    # Part 2
+
+def second(input) -> int | float:
     sub = Submarine(use_aim=True)
-    sub.parse_instructions(instructions)
-    print("Answer 2: ", sub.pos * sub.depth)
+    sub.parse_instructions(instructions=input)
+    return sub.pos * sub.depth
 
 
 if __name__ == "__main__":
-    main()
+    original_input = load_input_data(file.parent / "input.txt", day=1, year=2021)
+    input_ = preprocess_input(original_input)
+    print("The answer to part 1 is:", first(input_))
+    print("The answer to part 2 is:", second(input_))
