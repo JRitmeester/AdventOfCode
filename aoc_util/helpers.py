1696595ce547b2e5e56001ee3379235cc2834c63
JRitmeester <jeroen.c.ritmeester@gmail.com>
2022-11-25 21:20:06 +0100

    Move assert_session_id function to helpers.py

--- aoc_util/helpers.py
+++ aoc_util/helpers.py
@@ -1,9 +1,29 @@
+import shutil
 import time
 from functools import wraps
 from pathlib import Path
 
 import numpy as np
 from aocd import get_data
+
+
+def assert_session_id() -> bool:
+    """
+    Ensures the Session ID is set in $HOME/.config/aocd/token, from which aocd retrieves it automatically. If it is
+    not set already, this function will try to retrieve it from the .session_id file from the root of the project
+    directory.
+    """
+    session_id_file = Path("~").expanduser() / ".config/aocd/token"
+    if not session_id_file.exists():
+        session_id_local = Path.cwd() / ".session_id"
+        if session_id_local.exists():
+            session_id_file.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy(session_id_local, session_id_file)
+        else:
+            print(
+                f"No session ID could be found in {session_id_file.as_posix()} or {session_id_local.as_posix()}."
+            )
+    return session_id_file.exists()
 
 
 def print_timing(func: callable) -> callable:
