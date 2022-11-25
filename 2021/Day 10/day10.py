29c841c39b8d4bb6d17f0d57ae0b7d2616b7f34c
JRitmeester <jeroen.c.ritmeester@gmail.com>
2022-11-25 23:11:13 +0100

    Reformat Day 10/2021 with test

--- 2021/Day 10/day10.py
+++ 2021/Day 10/day10.py
@@ -1,34 +1,40 @@
-from pathlib import Path
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from pprint import pprint
 import re
+import sys
+from pathlib import Path
+
+import numpy as np
+
+# Add the repository root the sys.path in order to import the helper modules.
+file = Path(__file__)
+REPO_ROOT = next(
+    (parent for parent in file.parents if parent.name.lower() == "advent of code"),
+    None,
+)
+sys.path.append(REPO_ROOT.as_posix())
+
+from aoc_util.helpers import load_input_data
 
 
 def remove_kernels(line):
-    kernels = ["()", "{}", "[]", "<>"]
-
+    matching_brackets = r"\{\}|\(\)|\[\]|\<\>"
     # Keep searching for kernels if they're there.
-    while np.array((re.findall(r"(\(\))|(\[\])|(<>)|({})", line))).size != 0:
-        for kernel in kernels:
-            # Remove any found kernels.
-            line = re.sub(r"(\(\))|(\[\])|(<>)|({})", "", line)
+    while np.array((re.findall(matching_brackets, line))).size != 0:
+        # Remove any found kernels.
+        line = re.sub(matching_brackets, "", line)
 
     return line
 
 
-def main():
-    input_raw = (Path(__file__).parent / "input.txt").read_text()
-    lines = np.array(input_raw.split("\n")[:-1])
+def preprocess_input(input_text: str):
+    return np.array(input_text.split("\n"))
 
-    incomplete = []  # For part 2
 
-    # Part 1
+def first(input) -> int:
     total = 0
+    incomplete = []
     scores = {")": 3, "]": 57, "}": 1197, ">": 25137}
 
-    for i, line in enumerate(lines):
+    for line in input:
         copy = line
         copy = remove_kernels(copy)
         illegal_chars = re.findall("\]|\)|>|}", copy)
@@ -36,24 +42,27 @@ def main():
             total += scores[illegal_chars[0]]
         else:
             incomplete.append(copy)
+    return total, incomplete
 
-    print("Answer part 1:", total)
 
-    # Part 2
+def second(incomplete_lines: list[str]) -> int:
     totals = []
     scores = {")": 1, "]": 2, "}": 3, ">": 4}
     opposites = {"[": "]", "<": ">", "(": ")", "{": "}"}
 
-    for line in incomplete:
-        # print(line)
+    for line in incomplete_lines:
         total = 0
         mirrored = "".join([opposites[char] for char in reversed(line)])
         for char in mirrored:
             total = total * 5 + scores[char]
         totals.append(total)
 
-    print("Answer part 2:", int(np.median(totals)))
+    return int(np.median(totals))
 
 
 if __name__ == "__main__":
-    main()
+    original_input = load_input_data(file.parent / "input.txt", day=10, year=2021)
+    preprocessed_input = preprocess_input(original_input)
+    answer_first, incomplete = first(preprocessed_input)
+    print("The answer to part 1 is:", answer_first)
+    print("The answer to part 2 is:", second(incomplete))
---
+++ 2021/Day 10/day10_test.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from pathlib import Path
+
+import pytest
+
+# Add the repository root the sys.path in order to import the helper modules.
+file = Path(__file__)
+REPO_ROOT = next(
+    (parent for parent in file.parents if parent.name == "Advent of Code"),
+    None,
+)
+sys.path.append(REPO_ROOT.as_posix())
+import day10 as day
+
+from aoc_util.helpers import load_input_data
+
+
+@pytest.fixture
+def preprocessed_input():
+    input = load_input_data(file.parent / "example_input.txt", day=10, year=2021)
+    return day.preprocess_input(input)
+
+
+def test_first(preprocessed_input):
+    result, _ = day.first(preprocessed_input)
+    assert result == 26397
+
+
+def test_second(preprocessed_input):
+    _, incomplete_lines = day.first(preprocessed_input)
+    result = day.second(incomplete_lines)
+    assert result == 288957
+
+
+if __name__ == "__main__":
+    os.system(f'pytest "{file}" -s')
