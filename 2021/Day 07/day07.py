8a869bbc11c8a97d5c2eaf4648d991006d93dc88
JRitmeester <jeroen.c.ritmeester@gmail.com>
2022-11-25 16:25:42 +0100

    Reformat Day 7/2021 with test

--- 2021/Day 07/day07.py
+++ 2021/Day 07/day07.py
@@ -1,35 +1,48 @@
+import sys
 from pathlib import Path
+
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import math
+
+# Add the repository root the sys.path in order to import the helper modules.
+file = Path(__file__)
+REPO_ROOT = next(
+    (parent for parent in file.parents if parent.name.lower() == "advent of code"),
+    None,
+)
+sys.path.append(REPO_ROOT.as_posix())
+
+from aoc_util.helpers import load_input_data
 
 
-def get_nth_triangle_number(n):
+def get_nth_triangle_number(n: int) -> int:
     """
     Triangle numbers are for summing what factorials are for multiplication.
     """
     return (n * n + n) / 2
 
 
-def main():
-    input_raw = (Path(__file__).parent / "input.txt").read_text()
-    positions = np.array([int(x) for x in input_raw.split(",")])
+def preprocess_input(original_input) -> np.ndarray:
+    positions = np.array([int(x) for x in original_input.split(",")])
+    return positions
 
-    # Part 1: Calculate the total distance travelled by all the crabs, and find the
-    # minimal fuel usage.
-    fuel_usages = np.sum([np.abs(positions - x) for x in range(max(positions))], axis=1)
-    best_position, minimal_fuel_usage = np.argmin(fuel_usages), np.min(fuel_usages)
-    print(f"The minimal fuel usage (part 1) is {minimal_fuel_usage}")
 
-    # Part 2:
+def first(input) -> int:
+    fuel_usages = np.sum([np.abs(input - x) for x in range(max(input))], axis=1)
+    minimal_fuel_usage = np.min(fuel_usages)
+    return minimal_fuel_usage
+
+
+def second(input) -> int:
     fuel_usages = np.sum(
-        [get_nth_triangle_number(np.abs(positions - x)) for x in range(max(positions))],
+        [get_nth_triangle_number(np.abs(input - x)) for x in range(max(input))],
         axis=1,
     )
-    best_position, minimal_fuel_usage = np.argmin(fuel_usages), np.min(fuel_usages)
-    print(f"The minimal fuel usage (part 2) is {minimal_fuel_usage}")
+    minimal_fuel_usage = int(np.min(fuel_usages))
+    return minimal_fuel_usage
 
 
 if __name__ == "__main__":
-    main()
+    original_input = load_input_data(file.parent / "input.txt", day=1, year=2021)
+    preprocessed_input = preprocess_input(original_input)
+    print("The answer to part 1 is:", first(preprocessed_input))
+    print("The answer to part 2 is:", second(preprocessed_input))
