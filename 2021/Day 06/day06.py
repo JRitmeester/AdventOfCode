3805fca3ed27b034de4ed6b2bee2c69174fd65de
JRitmeester <jeroen.c.ritmeester@gmail.com>
2022-11-25 16:16:02 +0100

    Resolve Day 6/2021 with test

--- 2021/Day 06/day06.py
+++ 2021/Day 06/day06.py
@@ -1,57 +1,51 @@
-import numpy as np
+import sys
 from pathlib import Path
-import matplotlib.pyplot as plt
-from pprint import pprint
+
+import numpy as np
+
+# Add the repository root the sys.path in order to import the helper modules.
+file = Path(__file__)
+REPO_ROOT = next(
+    (parent for parent in file.parents if parent.name.lower() == "advent of code"),
+    None,
+)
+sys.path.append(REPO_ROOT.as_posix())
+
+from aoc_util.helpers import load_input_data
 
 
-def create_map(lines: list, include_diagonal: bool):
-    grid = np.zeros((2000, 2000))
-
-    max_x = 0
-    max_y = 0
-
-    for line in lines:
-        start = np.array(line[0])
-        end = np.array(line[1])
-        delta = np.array([end[0] - start[0], end[1] - start[1]])
-        length = np.max(np.abs(delta))
-        dir = delta / length
-
-        if not include_diagonal:
-            if np.sum(np.abs(dir)) != 1:
-                continue
-
-        for step in range(length + 1):
-            current = (start + step * dir).astype(np.int)
-            grid[current[1], current[0]] += 1
-            max_x = max(max_x, current[0])
-            max_y = max(max_y, current[1])
-
-    grid = grid[:max_y, :max_x]
-
-    return grid
+def preprocess_input(input_text: str):
+    lanternfish_dict = {
+        int(days): input_text.count(days) for days in set(input_text) if days != ","
+    }
+    return lanternfish_dict
 
 
-def main():
-    input_raw = (Path(__file__).parent / "input.txt").read_text()
-    input_split = input_raw.split("\n")[:-1]
-    lines = [
-        [tuple(int(el) for el in point.split(",")) for point in line.split(" -> ")]
-        for line in input_split
-    ]
+def evolve(lanternfish: dict, days: int) -> dict:
+    for day in range(days):
+        new_lanternfish_dict = {}
+        for days_left, num_fish in lanternfish.items():
+            new_lanternfish_dict[days_left - 1] = num_fish
+        new_lanternfish = new_lanternfish_dict.get(-1, 0)
+        new_lanternfish_dict[-1] = 0
+        new_lanternfish_dict[8] = new_lanternfish_dict.get(8, 0) + new_lanternfish
+        new_lanternfish_dict[6] = new_lanternfish_dict.get(6, 0) + new_lanternfish
+        lanternfish = new_lanternfish_dict
+    return lanternfish
 
-    # Part 1
-    grid = create_map(lines, include_diagonal=False)
-    plt.imshow(grid)
-    print("Answer part 1:", np.count_nonzero(grid > 1))
 
-    # Part 2
-    grid = create_map(lines, include_diagonal=True)
-    plt.imshow(grid)
-    print("Answer part 2:", np.count_nonzero(grid > 1))
+def first(input) -> int:
+    lanternfish = evolve(input, days=80)
+    return sum(lanternfish.values())
 
-    plt.show()
+
+def second(input) -> int:
+    lanternfish = evolve(input, days=256)
+    return sum(lanternfish.values())
 
 
 if __name__ == "__main__":
-    main()
+    original_input = load_input_data(file.parent / "input.txt", day=1, year=2021)
+    preprocessed_input = preprocess_input(original_input)
+    print("The answer to part 1 is:", first(preprocessed_input))
+    print("The answer to part 2 is:", second(preprocessed_input))
