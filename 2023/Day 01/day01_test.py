41c560fcbff74b4d5a9d9d75829d7e589bade78b
JRitmeester <jeroen.c.ritmeester@gmail.com>
2023-12-01 15:52:06 +0100

    Day 1: Trebuchet?!

---
+++ 2023/Day 01/day01_test.py
@@ -0,0 +1,45 @@
+import os
+import sys
+from pathlib import Path
+
+import pytest
+
+# Add the repository root the sys.path in order to import the helper modules.
+file = Path(__file__)
+REPO_ROOT = next(
+    (parent for parent in file.parents if parent.name == "Advent of Code"),
+    None,
+)
+sys.path.append(REPO_ROOT.as_posix())
+import day01 as day
+
+from aoc_util.helpers import load_input_data
+
+
+@pytest.fixture
+def preprocessed_input_1():
+    input = load_input_data(
+        file.parent / "example_input_1.txt", day=1, year=2023
+    )
+    return day.preprocess_input(input)
+
+@pytest.fixture
+def preprocessed_input_2():
+    input = load_input_data(
+        file.parent / "example_input_2.txt", day=1, year=2023
+    )
+    return day.preprocess_input(input)
+
+
+def test_first(preprocessed_input_1):
+    result = day.first(preprocessed_input_1)
+    assert result == 142
+
+
+def test_second(preprocessed_input_2):
+    result = day.second(preprocessed_input_2)
+    assert result == 281
+
+
+if __name__ == "__main__":
+    os.system(f'pytest "{file}" -s')
