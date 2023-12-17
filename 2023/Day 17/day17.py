611c225556e0f7691a8f7acf2d2f958bfc77af73
JRitmeester <jeroen.c.ritmeester@gmail.com>
2023-12-17 10:08:08 +0100

    Day 17: Clumsy Crucible

---
+++ 2023/Day 17/day17.py
@@ -0,0 +1,4 @@
+print("""Fuck pathfinding. No one ever woke up and thought:
+'Yay, we get to copy paste another A* algorithm and modify it to fit some weird constraints!'.
+No. Stop. Go outside and get a coffee or something. Call your parents. Do something that is actually rewarding.
+""")
